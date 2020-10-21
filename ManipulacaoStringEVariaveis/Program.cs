031691caac416c5bdae2fb44f02ecec95d89d210
Bruno Costa <brunoarruda697@gmail.com>
2020-10-21 10:57:54 -0300

    Manipulação de strings

---
+++ ManipulacaoStringEVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ManipulacaoStringEVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 23;
+            double saldo = 10.35784;
+            string nome = "Bruno";
+
+            // **Placeholders**
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais", nome, idade, saldo);
+            // dá pra usar arredondamento de float dentro das chaves.
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            // **Interpolação**
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            // **Concatenação**
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
+            // só da pra utilizar o objeto CultureInfo na forma de concatenação
+        }
+    }
+}
